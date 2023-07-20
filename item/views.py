aaa13e0e3fb0a7bec46dda080d81d3ed84cf5f94
TBS5596 <thomsimbeye99@gmail.com>
2023-07-20 08:58:44 +0200

    Update views.py

--- item/views.py
+++ item/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 
 from django.db.models import Q
+from django.db.models import Count
 
 from.forms import NewItemForm, EditItemForm
 
